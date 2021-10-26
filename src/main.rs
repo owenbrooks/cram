e3f38415b56ae3ed6416ed78b4c3bb557ee42190
owenbrooks <owen.h.brooks@gmail.com>
2021-10-26 18:39:54 +1100

    Draw point cloud

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,34 @@
 use ndarray::prelude::*;
+use ndarray::{stack};
 use nannou::prelude::*;
-use nannou::image::io::Reader as ImageReader;
 fn main() {
-    let x: Array2<f64> = Array::zeros((3,3));
-    println!("{}", x);
     nannou::app(model).update(update).run();
 }
 
 struct Model {
     mouse_pos: Vec2,
-    texture: wgpu::Texture,
+    clouds: Vec<Array2<f32>>,
 }
 
 fn model(app: &App) -> Model {
     app.new_window()
         .title("Cram")
-        .size(1200+20, 567+20)
         .event(event)
         .view(view)
         .build()
         .unwrap();
 
-    let assets = app.assets_path().unwrap();
-    let img_path = assets.join("maps").join("floor.jpg");
-    let texture = wgpu::Texture::from_path(app, img_path.clone()).unwrap();
-    let img = ImageReader::open(img_path).unwrap().decode().unwrap().to_rgb8();
-    // let pixels = img.as_raw().to_vec();
-    // let matrix = Array::from_shape_vec((img.height() as usize, img.width() as usize, 3 as usize), pixels).unwrap();
-    // println!("{:?}", matrix.shape());
+    let x = Array::linspace(-3., 3., 100);
+    let y = x.map(|t| t.sin());
+    let pc = stack![Axis(1), x, y];
 
     Model {
         mouse_pos: pt2(0.0, 0.0),
-        texture,
+        clouds: vec![pc],
     }
 }
 
-fn update(_app: &App, model: &mut Model, _update: Update) {
+fn update(_app: &App, _model: &mut Model, _update: Update) {
 
 }
 
@@ -49,13 +42,17 @@ fn event(_app: &App, model: &mut Model, event: WindowEvent) {
 }
 
 fn view(app: &App, model: &Model, frame: Frame) {
-    // Prepare to draw.
     let draw = app.draw();
-
-    // Clear the background to purple.
     draw.background().color(WHITE);
-    draw.texture(&model.texture);
-    // println!("{}", model.mouse_pos);
+
+    let m2pixel = 100.0;
+    for pc in &model.clouds {
+        for row in pc.outer_iter() {
+            let x = row[0]*m2pixel;
+            let y = row[1]*m2pixel;
+            draw.ellipse().x_y(x, y).radius(3.0).color(nannou::color::BLACK);
+        }
+    }
 
     draw.to_frame(app, &frame).unwrap();
 }
