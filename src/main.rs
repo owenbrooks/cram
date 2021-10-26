f7eea177dd36ab5da074b69371d2d073f0be19e5
owenbrooks <owen.h.brooks@gmail.com>
2021-10-26 12:56:32 +1100

    Read in and display image

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use ndarray::prelude::*;
 use nannou::prelude::*;
+use nannou::image::io::Reader as ImageReader;
 fn main() {
-    println!("Hello, world!");
     let x: Array2<f64> = Array::zeros((3,3));
     println!("{}", x);
     nannou::app(model).update(update).run();
@@ -9,6 +9,7 @@ fn main() {
 
 struct Model {
     mouse_pos: Vec2,
+    texture: wgpu::Texture,
 }
 
 fn model(app: &App) -> Model {
@@ -19,8 +20,17 @@ fn model(app: &App) -> Model {
         .build()
         .unwrap();
 
+    let assets = app.assets_path().unwrap();
+    let img_path = assets.join("maps").join("floor.jpg");
+    let texture = wgpu::Texture::from_path(app, img_path.clone()).unwrap();
+    let img = ImageReader::open(img_path).unwrap().decode().unwrap().to_rgb8();
+    let pixels = img.as_raw().to_vec();
+    let matrix = Array::from_shape_vec((img.height() as usize, img.width() as usize, 3 as usize), pixels).unwrap();
+    println!("{:?}", matrix.shape());
+
     Model {
         mouse_pos: pt2(0.0, 0.0),
+        texture,
     }
 }
 
@@ -43,6 +53,8 @@ fn view(app: &App, model: &Model, frame: Frame) {
 
     // Clear the background to purple.
     draw.background().color(WHITE);
+    draw.texture(&model.texture);
+    // println!("{}", model.mouse_pos);
 
     draw.to_frame(app, &frame).unwrap();
 }
