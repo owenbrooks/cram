4e2297d1053ffa1264a162ca620a871512b0868c
owenbrooks <owen.h.brooks@gmail.com>
2021-10-28 12:01:30 +1100

    Add drawing of corresponding point pairs

--- src/icp.rs
+++ src/icp.rs
@@ -4,16 +4,20 @@ use kdtree::distance::squared_euclidean;
 
 // input: two point clouds, reference (dimension mxn) and new (dimension wxn)
 // output: vector of indices of the points in reference that are closest to the points in new, length w
-fn nearest_neighbours(reference: Array2<f32>, new: Array2<f32>) -> Array1<usize> {
+pub fn nearest_neighbours(reference: &Array2<f32>, new: &Array2<f32>) -> Array1<usize> {
 	let dimensions = reference.ncols() - 1;
 	let mut kdtree = KdTree::new(dimensions);
-	for i in 0..reference.len() {
+	for i in 0..reference.nrows() {
 		kdtree.add([reference[[i, 0]], reference[[i, 1]]], i).unwrap();
 	}
 	let mut result = Vec::new();
-	for i in 0..new.len() {
+	for i in 0..new.nrows() {
 		let nearest = kdtree.nearest(&[new[[i, 0]], new[[i, 1]]], i, &squared_euclidean);
-		result.push(*nearest.unwrap().first().unwrap().1);
+		if let Ok(nearest) = nearest {
+			if let Some(nearest) = nearest.first() {
+				result.push(*nearest.1);
+			}
+		}
 	}
 	Array1::from_vec(result)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 use ndarray::prelude::*;
 use ndarray::stack;
 use nannou::prelude::*;
+use cram::icp::nearest_neighbours;
 fn main() {
     nannou::app(model).update(update).run();
 }
 
 struct Model {
     mouse_pos: Vec2,
-    clouds: Vec<Array2<f32>>,
+    cloud_ref: Array2<f32>,
+    cloud_target: Array2<f32>,
+    correspondences: Array1<usize>,
 }
 
 fn model(app: &App) -> Model {
@@ -21,7 +24,7 @@ fn model(app: &App) -> Model {
     let x = Array::linspace(-3., 3., 100);
     let y = x.map(|t| t.sin());
     let h = Array::ones(x.len());
-    let pc = stack![Axis(1), x, y, h];
+    let cloud_ref = stack![Axis(1), x, y, h];
 
     let rmat = cram::transforms::angle_to_rmat(std::f32::consts::FRAC_PI_6);
     let trans = array![2., 0.];
@@ -30,16 +33,20 @@ fn model(app: &App) -> Model {
                     [0., 0., 1.],
                 ];
 
-    let mut pc2 = Array2::zeros((pc.nrows(), pc.ncols()));
-    for (i, mut row) in pc2.axis_iter_mut(Axis(0)).enumerate() {
-        let pt = pc.slice(s![i, ..]);
+    let mut cloud_target = Array2::zeros((cloud_ref.nrows(), cloud_ref.ncols()));
+    for (i, mut row) in cloud_target.axis_iter_mut(Axis(0)).enumerate() {
+        let pt = cloud_ref.slice(s![i, ..]);
         let new_pt = t.dot(&pt);
         row.assign(&new_pt);
     }
 
+    let correspondences = nearest_neighbours(&cloud_ref, &cloud_target);
+
     Model {
         mouse_pos: pt2(0.0, 0.0),
-        clouds: vec![pc, pc2],
+        cloud_ref,
+        cloud_target, 
+        correspondences,
     }
 }
 
@@ -61,12 +68,26 @@ fn view(app: &App, model: &Model, frame: Frame) {
     draw.background().color(WHITE);
 
     let m2pixel = 100.0;
-    for pc in &model.clouds {
-        for row in pc.outer_iter() {
-            let x = row[0]*m2pixel;
-            let y = row[1]*m2pixel;
-            draw.ellipse().x_y(x, y).radius(3.0).color(nannou::color::BLACK);
-        }
+    for row in model.cloud_ref.outer_iter() {
+        let x = row[0]*m2pixel;
+        let y = row[1]*m2pixel;
+        draw.ellipse().x_y(x, y).radius(3.0).color(nannou::color::BLACK);
+    }
+    for row in model.cloud_target.outer_iter() {
+        let x = row[0]*m2pixel;
+        let y = row[1]*m2pixel;
+        draw.ellipse().x_y(x, y).radius(3.0).color(nannou::color::MEDIUMSLATEBLUE);
+    }
+
+    for to in 0..model.correspondences.len() {
+        let from = model.correspondences[to];
+        let new_pt = model.cloud_ref.index_axis(Axis(0), from);
+        let ref_pt = model.cloud_target.index_axis(Axis(0), to);
+
+        let new_pt = vec2(new_pt[0], new_pt[1])*m2pixel;
+        let ref_pt = vec2(ref_pt[0], ref_pt[1])*m2pixel;
+
+        draw.line().points(new_pt, ref_pt).color(nannou::color::LIME);
     }
 
     draw.to_frame(app, &frame).unwrap();
