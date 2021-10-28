8ec33158ddaf096ae9a5c754c5560f7bbed7523c
owenbrooks <owen.h.brooks@gmail.com>
2021-10-28 20:17:03 +1100

    Initial ICP implementation

--- src/icp.rs
+++ src/icp.rs
@@ -1,10 +1,13 @@
 use ndarray::prelude::*;
 use kdtree::KdTree;
 use kdtree::distance::squared_euclidean;
+use optimization_engine::{SolverError, Problem, panoc::{PANOCCache, PANOCOptimizer}, constraints};
+use optimization_engine::*;
+use crate::transforms::*;
 
 // input: two point clouds, reference (dimension mxn) and new (dimension wxn)
 // output: vector of indices of the points in reference that are closest to the points in new, length w
-pub fn nearest_neighbours(reference: &Array2<f32>, new: &Array2<f32>) -> Array1<usize> {
+pub fn nearest_neighbours(reference: &Array2<f64>, new: &Array2<f64>) -> Array1<usize> {
 	let dimensions = reference.ncols() - 1;
 	let mut kdtree = KdTree::new(dimensions);
 	for i in 0..reference.nrows() {
@@ -20,4 +23,84 @@ pub fn nearest_neighbours(reference: &Array2<f32>, new: &Array2<f32>) -> Array1<
 		}
 	}
 	Array1::from_vec(result)
+}
+
+fn sum_squared_distance(reference: &Array2<f64>, target: &Array2<f64>, correspondences: &Array1<usize>, transform: &Array2<f64>) -> f64 {
+	let mut sum = 0.;
+	for i in 0..correspondences.len() {
+		let q = target.row(i);
+		let to = correspondences[i];
+		let p = reference.row(to).to_owned();
+		let tq = transform.dot(&q);
+		let diff = p - tq;
+		let cost = diff.dot(&diff);
+		sum += cost;
+	}
+	sum
+}
+fn sum_squared_distance_grad(reference: &Array2<f64>, target: &Array2<f64>, correspondences: &Array1<usize>, transform: &Array2<f64>, grad: &mut [f64]) {
+	// println!("{:?}", transform);
+	for i in 0..correspondences.len() {
+		let q = target.row(i);
+		let to = correspondences[i];
+		let p = reference.row(to).to_owned();
+		let tq = transform.dot(&q);
+		let diff = p - tq;
+		let sinw = transform[[0, 1]];
+		let cosw = transform[[0, 0]];
+		let dtdu = array![[-sinw, -cosw, 1.], [cosw, -sinw, 1.], [0., 0., 0.]];
+		let elem = -2.0*(diff)*dtdu.dot(&q);
+
+		grad[0] += elem[0];
+		grad[1] += elem[1];
+		grad[2] += elem[2];
+	}
+}
+
+pub fn find_transform(reference: &Array2<f64>, target: &Array2<f64>, correspondences: &Array1<usize>) -> Array2<f64> {
+	/* USER PARAMETERS */
+    let tolerance = 1e-14;
+    let n = 3;
+    let lbfgs_memory = 10;
+    let max_iters = 80;
+    let mut u = [0., 0., 0.]; // initial guess
+	
+	let f = |u: &[f64], c: &mut f64| -> Result<(), SolverError> {
+		let w = u[0];
+		let t = array![u[1], u[2]];
+		let rmat = angle_to_rmat(w);
+		let tmat = rmat_and_tvec_to_tmat(&rmat, &t);
+		*c = sum_squared_distance(reference, target, correspondences, &tmat);
+		Ok(())
+	};
+
+	let df = |u: &[f64], grad: &mut [f64]| -> Result<(), SolverError> {
+		let w = u[0];
+		let t = array![u[1], u[2]];
+		let rmat = angle_to_rmat(w);
+		let tmat = rmat_and_tvec_to_tmat(&rmat, &t);
+        sum_squared_distance_grad(reference, target, correspondences, &tmat, grad);
+        Ok(())
+    };
+
+	// define the constraints
+    let bounds = constraints::NoConstraints::new();
+
+    /* PROBLEM STATEMENT */
+    let problem = Problem::new(&bounds, df, f);
+    let mut panoc_cache = PANOCCache::new(n, tolerance, lbfgs_memory);
+    let mut panoc = PANOCOptimizer::new(problem, &mut panoc_cache)
+            .with_max_iter(max_iters);
+
+    // Invoke the solver
+    let status = panoc.solve(&mut u);
+
+	println!("{:?}", status);
+
+	let w = u[0];
+	let t = array![u[1], u[2]];
+	let rmat = angle_to_rmat(w);
+	let tmat = rmat_and_tvec_to_tmat(&rmat, &t);
+
+	tmat
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use ndarray::prelude::*;
 use ndarray::stack;
+use ndarray_linalg::solve::Inverse;
 use nannou::prelude::*;
 use cram::icp::nearest_neighbours;
 fn main() {
@@ -8,9 +9,11 @@ fn main() {
 
 struct Model {
     mouse_pos: Vec2,
-    cloud_ref: Array2<f32>,
-    cloud_target: Array2<f32>,
+    cloud_ref: Array2<f64>,
+    cloud_target: Array2<f64>,
+    computed_transform: Array2<f64>,
     correspondences: Array1<usize>,
+    should_step: bool,
 }
 
 fn model(app: &App) -> Model {
@@ -26,16 +29,11 @@ fn model(app: &App) -> Model {
     let h = Array::ones(x.len());
     let cloud_ref = stack![Axis(1), x, y, h];
 
-    let rmat = cram::transforms::angle_to_rmat(std::f32::consts::FRAC_PI_6);
-    let tvec = array![2., 0.];
-    let t = cram::transforms::rmat_and_tvec_to_tmat(rmat, tvec);
+    let rmat = cram::transforms::angle_to_rmat(std::f64::consts::FRAC_PI_6);
+    let tvec = array![0., 0.];
+    let tmat = cram::transforms::rmat_and_tvec_to_tmat(&rmat, &tvec);
 
-    let mut cloud_target = Array2::zeros((cloud_ref.nrows(), cloud_ref.ncols()));
-    for (i, mut row) in cloud_target.axis_iter_mut(Axis(0)).enumerate() {
-        let pt = cloud_ref.slice(s![i, ..]);
-        let new_pt = t.dot(&pt);
-        row.assign(&new_pt);
-    }
+    let cloud_target = cram::transforms::transformed_cloud(&cloud_ref, &tmat);
 
     let correspondences = nearest_neighbours(&cloud_ref, &cloud_target);
 
@@ -43,19 +41,34 @@ fn model(app: &App) -> Model {
         mouse_pos: pt2(0.0, 0.0),
         cloud_ref,
         cloud_target, 
+        computed_transform: Array::eye(tvec.len()+1), // will eventually bring ref to target
+        // computed_transform: tmat,
         correspondences,
+        should_step: false,
     }
 }
 
-fn update(_app: &App, _model: &mut Model, _update: Update) {
+fn update(_app: &App, model: &mut Model, _update: Update) {
+    if model.should_step {
+        model.should_step = false;
+        // Perform one ICP iteration and update transform
+        let transformed_target = cram::transforms::transformed_cloud(&model.cloud_ref, &model.computed_transform);
+        let new_transform = cram::icp::find_transform(&model.cloud_ref, &transformed_target, &model.correspondences);
+        model.computed_transform = new_transform.dot(&model.computed_transform);
 
+        println!("{:?}", model.computed_transform);
+        // Update correspondences
+        let transformed_target = cram::transforms::transformed_cloud(&model.cloud_ref, &model.computed_transform);
+        model.correspondences = nearest_neighbours(&model.cloud_ref, &transformed_target);
+    }
 }
 
 fn event(_app: &App, model: &mut Model, event: WindowEvent) {
     match event {
         WindowEvent::MouseMoved(pos) => {
             model.mouse_pos = pos;
-        }
+        },
+        KeyPressed(Key::Right) => model.should_step = true,
         _other => (),
     }
 }
@@ -64,28 +77,30 @@ fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
     draw.background().color(WHITE);
 
-    let m2pixel = 100.0;
+    let m2pixel: f64 = 100.0;
 
     // Draw point clouds
     for row in model.cloud_ref.outer_iter() {
         let x = row[0]*m2pixel;
         let y = row[1]*m2pixel;
-        draw.ellipse().x_y(x, y).radius(3.0).color(nannou::color::BLACK);
+        draw.ellipse().x_y(x as f32, y as f32).radius(3.0).color(nannou::color::BLACK);
     }
-    for row in model.cloud_target.outer_iter() {
+    let inverse_transform = &model.computed_transform.inv().unwrap();
+    let transformed_target = cram::transforms::transformed_cloud(&model.cloud_target, &inverse_transform);
+    for row in transformed_target.outer_iter() {
         let x = row[0]*m2pixel;
         let y = row[1]*m2pixel;
-        draw.ellipse().x_y(x, y).radius(3.0).color(nannou::color::MEDIUMSLATEBLUE);
+        draw.ellipse().x_y(x as f32, y as f32).radius(3.0).color(nannou::color::MEDIUMSLATEBLUE);
     }
 
     // Draw lines to indicate correspondences
     for to in 0..model.correspondences.len() {
         let from = model.correspondences[to];
         let new_pt = model.cloud_ref.index_axis(Axis(0), from);
-        let ref_pt = model.cloud_target.index_axis(Axis(0), to);
+        let ref_pt = transformed_target.index_axis(Axis(0), to);
 
-        let new_pt = vec2(new_pt[0], new_pt[1])*m2pixel;
-        let ref_pt = vec2(ref_pt[0], ref_pt[1])*m2pixel;
+        let new_pt = vec2(new_pt[0] as f32, new_pt[1] as f32)*m2pixel as f32;
+        let ref_pt = vec2(ref_pt[0] as f32, ref_pt[1] as f32)*m2pixel as f32;
 
         draw.line().points(new_pt, ref_pt).color(nannou::color::LIME);
     }
